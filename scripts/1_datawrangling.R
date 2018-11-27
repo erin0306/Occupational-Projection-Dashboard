33053caa26b75ad03555cf3e23b0b204a8bf4088
DANIELLA MESLER <daniellamesler@D-10-18-161-249.dhcp4.washington.edu>
2018-11-26 17:05:23 -0800

    started some basic statistics, downloaded more data

--- scripts/1_datawrangling.R
+++ scripts/1_datawrangling.R
@@ -1,5 +1,27 @@
-#read in data
-occupational_data_by_state <- read.csv("data/edited_occupation_by_state.csv")
+#read in data occupational data by state
+original_occupational_data_by_state <- read_excel("data/occupational_data_by_state.xlsx")
 
-## finding higer annual mean wages attempt!
-highest_a_mean <- filter(occupational_data_by_state, a_mean >= "10000")
+## Get rid of null A_MEAN values
+occupational_data_by_state <- filter(original_occupational_data_by_state, A_MEAN != "*" & A_MEAN != "#")
+
+## WHY ARE THE WAGES NOT FOLLOWING THE MAX/MIN??? the highest wage is greater than 200,000?????
+## Minimum annual mean wage in each state
+min_annual_wage_per_state <- select(occupational_data_by_state, ST, STATE, OCC_CODE, OCC_TITLE, TOT_EMP, A_MEAN) %>%
+  group_by(STATE) %>%
+  filter(A_MEAN == min(A_MEAN)
+  )
+
+## Maximum annual mean wage in each state
+max_annual_wage_per_state <- select(occupational_data_by_state, ST, STATE, OCC_CODE, OCC_TITLE, TOT_EMP, A_MEAN) %>%
+  group_by(STATE) %>%
+  filter(A_MEAN == max(A_MEAN)
+  )
+
+## Categorizing by annual wages IN PROCESS
+a_mean_test <- select(occupational_data_by_state, ST, STATE, OCC_CODE, OCC_TITLE, TOT_EMP, A_MEAN) %>%
+  group_by(OCC_TITLE) %>%
+  filter(A_MEAN >= "200000")
+
+
+#...... continue this
+
---
+++ scripts/2_datawrangling.R
@@ -0,0 +1,1 @@
+#read in data Occupational projections, there are several tables in this file so we need to separate each one after we decide which ones we want 

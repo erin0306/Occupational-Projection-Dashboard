a9bca99f0f0f0fbabcee1a1f855e212e85b57a90
DANIELLA MESLER <daniellamesler@DANIELLAs-MacBook-Air-2.local>
2018-11-29 11:35:47 -0800

    add more basic stats

--- scripts/2_datawrangling.R
+++ scripts/2_datawrangling.R
@@ -1,1 +1,2 @@
-#read in data Occupational projections, there are several tables in this file so we need to separate each one after we decide which ones we want 
+#read in data Occupational projections, there are several tables in this file so we need to separate each 
+##one after we decide which ones we want 
